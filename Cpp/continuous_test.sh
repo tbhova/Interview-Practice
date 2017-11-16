9d4f60165729a7ddbf955b9a005914495637567f
Tyler Hovanec <tbhovanec@gmail.com>
2017-11-15 19:25:01 -0800

    Updated continuous test script to only run on file changes.

--- Cpp/continuous_test.sh
+++ Cpp/continuous_test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+hash_old=""
+
 while true
 do
-  cmake . && make && ./UnitTests
+  hash_new=$(md5sum $(ls | grep "\.h\|\.cpp\|\.hpp"))
+  if [[ $hash_new != $hash_old ]]
+  then
+  hash_old=$hash_new
+    cmake . && make && ./UnitTests
+  fi
   sleep 1
 done
