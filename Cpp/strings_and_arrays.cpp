01068b9a944301cdcbaf0a7088f1175907f26423
Tyler Hovanec <tbhovanec@student.ysu.edu>
2017-12-05 20:27:41 -0800

    Cleanup

--- Cpp/strings_and_arrays.cpp
+++ Cpp/strings_and_arrays.cpp
@@ -1,6 +1,9 @@
+#include "strings_and_arrays.h"
+
+#include <unordered_set>
+#include <bits/unordered_set.h>
 #include <vector>
 #include <queue>
-#include "string_permute.h"
 
 struct Letter {
   char c;
@@ -53,3 +56,27 @@ bool Permute(const std::string& in, std::string* out) {
     return true;
   }
 }
+
+
+bool IsUnique(const string &s) {
+  std::unordered_set<char> seen;
+  for (const char& c : s) {
+    if (seen.find(c) != seen.end()) {
+      return false;
+    }
+    seen.insert(c);
+  }
+  return true;
+}
+
+void Reverse(string& in) {
+  unsigned int end = in.size();
+  if (end == 0) { return; }
+  end--;
+  for (int i = 0; i < end; i++, end--) {
+    char tmp = in[i];
+    in[i] = in[end];
+    in[end] = tmp;
+  }
+}
+
